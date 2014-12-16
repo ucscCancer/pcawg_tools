f0b12f8f40a4c6ced2372ddfb6e2480e6db4f762
Kyle Ellrott <kellrott@gmail.com>
2014-12-15 16:20:20 -0800

    Adding more download info

--- data/download.sh
+++ data/download.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-
 #genomic data
 wget -r ftp://ftp.sanger.ac.uk/pub/project/PanCancer/
+mv ftp.sanger.ac.uk/pub/project/PanCancer/* ./
 
 #data for ContEst
-wget wget http://www.broadinstitute.org/~kcibul/contest/hg19_population_stratified_af_hapmap_3.3.vcf.gz
-gunzip hg19_population_stratified_af_hapmap_3.3.vcf.gz
+#wget http://www.broadinstitute.org/~kcibul/contest/hg19_population_stratified_af_hapmap_3.3.vcf.gz
+#gunzip hg19_population_stratified_af_hapmap_3.3.vcf.gz
+wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/SNP6.hg19.interval_list
+wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/hg19_population_stratified_af_hapmap_3.3.fixed.vcf
+wget http://www.broadinstitute.org/~gsaksena/arrayfree_ContEst/arrayfree_ContEst/gaf_20111020+broad_wex_1.1_hg19.bed
 
-
+#data for MuTect
 wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/b37_cosmic_v54_120711.vcf
 wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/dbsnp_132_b37.leftAligned.vcf.gz
 gunzip dbsnp_132_b37.leftAligned.vcf.gz
