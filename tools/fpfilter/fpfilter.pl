0b2bf1a5642ef1ed35a1588160ccbd5d6c1aed07
Dave Larson <dlarson@genome.wustl.edu>
2015-04-20 21:56:35 -0500

    remove unused function

--- tools/fpfilter/fpfilter.pl
+++ tools/fpfilter/fpfilter.pl
@@ -190,52 +190,6 @@ print $filtered_vcf @$vcf_header;
 print $filtered_vcf @vcf_lines;
 exit(0);
 
-
-################################################################################
-
-=head3	iupac_to_base
-
-    Convert IUPAC ambiguity codes to variant bases
-
-
-=cut
-
-
-sub iupac_to_base {
-    (my $allele1, my $allele2) = @_;
-
-    return($allele2) if($allele2 eq "A" || $allele2 eq "C" || $allele2 eq "G" || $allele2 eq "T");
-
-    if($allele2 eq "M") {
-        return("C") if($allele1 eq "A");
-        return("A") if($allele1 eq "C");
-        return("A");    ## Default for triallelic variant
-    } elsif($allele2 eq "R") {
-        return("G") if($allele1 eq "A");
-        return("A") if($allele1 eq "G");
-        return("A");     ## Default for triallelic variant
-    } elsif($allele2 eq "W") {
-        return("T") if($allele1 eq "A");
-        return("A") if($allele1 eq "T");
-        return("A");    ## Default for triallelic variant
-    } elsif($allele2 eq "S") {
-        return("C") if($allele1 eq "G");
-        return("G") if($allele1 eq "C");
-        return("C");    ## Default for triallelic variant
-    } elsif($allele2 eq "Y") {
-        return("C") if($allele1 eq "T");
-        return("T") if($allele1 eq "C");
-        return("C");    ## Default for triallelic variant
-    } elsif($allele2 eq "K") {
-        return("G") if($allele1 eq "T");
-        return("T") if($allele1 eq "G");
-        return("G");    ## Default for triallelic variant
-    }
-
-    return($allele2);
-}
-
-
 ################################################################################
 
 =head3	read_counts_by_allele
