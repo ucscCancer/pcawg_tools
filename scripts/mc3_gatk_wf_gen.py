145677190cb22e3bda4b0f6a1a6573659c1e4d10
Kyle Ellrott <kellrott@gmail.com>
2015-05-21 12:26:42 -0700

    Updating the MC3 GATK pipeline generation

--- scripts/mc3_gatk_wf_gen.py
+++ scripts/mc3_gatk_wf_gen.py
@@ -23,6 +23,8 @@ if __name__ == "__main__":
     parser.add_argument("--out-base", default="mc3_gatk")
     parser.add_argument("--ref-download", action="store_true", default=False)
     parser.add_argument("--create-service", action="store_true", default=False)
+    parser.add_argument("--scratch", default=None)
+
 
     args = parser.parse_args()
 
@@ -77,7 +79,7 @@ if __name__ == "__main__":
     workflow = GalaxyWorkflow(ga_file="workflows/Galaxy-Workflow-GATK_CGHub.ga")
 
     config = {
-        "table_id" : "syn3919407",
+        "table_id" : "syn4214588",
         "primary_col" : "participant_id",
         "assignee_col" : "assignee",
         "state_col" : "state"
@@ -86,24 +88,35 @@ if __name__ == "__main__":
     tasks = TaskGroup()
 
     for ent in synqueue.listAssignments(syn, **config):
-        task = GalaxyWorkflowTask("workflow_%s" % (ent['id']),
-            workflow,
-            inputs=dm,
-            parameters={
-                'normal_bam_download' : {
-                    "uuid" : ent['meta']['Normal_Analysis_ID'],
-                    "gnos_endpoint" : "https://cghub.ucsc.edu",
-                    "cred_file" : "/tool_data/files/cghub.key"
+        bam_set = list( a[0] for a in ent['meta'].items() if a[0].startswith("id_") and isinstance(a[1], basestring)  )
+        if len(bam_set) == 2:
+            task = GalaxyWorkflowTask("workflow_%s" % (ent['id']),
+                workflow,
+                inputs=dm,
+                parameters={
+                    'INPUT_BAM_1' : {
+                        "uuid" : bam_set[0],
+                        "gnos_endpoint" : "https://cghub.ucsc.edu",
+                        "cred_file" : "/tool_data/files/cghub.key"
+                    },
+                    'INPUT_BAM_2' : {
+                        "uuid" : bam_set[1],
+                        "gnos_endpoint" : "https://cghub.ucsc.edu",
+                        "cred_file" : "/tool_data/files/cghub.key"
+                    }
                 },
-                'tumor_bam_download' : {
-                    "uuid" : ent['meta']['Tumour_Analysis_ID'],
-                    "gnos_endpoint" : "https://cghub.ucsc.edu",
-                    "cred_file" : "/tool_data/files/cghub.key"
+                tags=[ "sample:%s" % (ent['meta']['participant_id']) ],
+                tool_tags = {
+                    "OUTPUT_BAM_1" : {
+                        "output_bam" : [ "original_bam:%s" % (bam_set[0]) ]
+                    },
+                    "OUTPUT_BAM_2" : {
+                        "output_bam" : [ "original_bam:%s" % (bam_set[1]) ]
+                    }
+
                 }
-            },
-            tags=[ "sample:%s" % (ent['meta']['participant_id']) ]
-        )
-        tasks.append(task)
+            )
+            tasks.append(task)
 
     if not os.path.exists("%s.tasks" % (args.out_base)):
         os.mkdir("%s.tasks" % (args.out_base))
@@ -124,4 +137,7 @@ if __name__ == "__main__":
             ]
         )
         with open("%s.service" % (args.out_base), "w") as handle:
-            service.get_config().store(handle)
+            s = service.get_config()
+            if args.scratch:
+                s.set_docstore_config(cache_dir=args.scratch)
+                s.store(handle)
